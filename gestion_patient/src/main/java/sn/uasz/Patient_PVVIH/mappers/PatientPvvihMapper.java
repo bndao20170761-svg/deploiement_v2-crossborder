11ff5b38741e421e34a66b03e96c99e28acc3e2b
Babacar Ndao <babacarndao1011@gmail.com>
2026-04-21 18:42:39 +0000

    mise à jour

--- gestion_patient/src/main/java/sn/uasz/Patient_PVVIH/mappers/PatientPvvihMapper.java
+++ gestion_patient/src/main/java/sn/uasz/Patient_PVVIH/mappers/PatientPvvihMapper.java
@@ -52,8 +52,8 @@ public interface PatientPvvihMapper {
 
     // DossierView (pour la réponse frontend)
     @Mapping(source = "patientCode", target = "codePatient")
-    @Mapping(target = "nomComplet", expression = "java(dossier.getPatient() != null ? dossier.getPatient().getNomUtilisateur() + ' ' + dossier.getPatient().getPrenomUtilisateur() : '')")
-    @Mapping(target = "doctorCreateNom", expression = "java(dossier.getDoctorCreate() != null && dossier.getDoctorCreate().getUtilisateur() != null ? dossier.getDoctorCreate().getUtilisateur().getNom() + ' ' + dossier.getDoctorCreate().getUtilisateur().getPrenom() : '')")
+    @Mapping(target = "nomComplet", expression = "java(dossier.getPatient() != null ? dossier.getPatient().getNomUtilisateur() + \" \" + dossier.getPatient().getPrenomUtilisateur() : \"\")")
+    @Mapping(target = "doctorCreateNom", expression = "java(dossier.getDoctorCreate() != null && dossier.getDoctorCreate().getUtilisateur() != null ? dossier.getDoctorCreate().getUtilisateur().getNom() + \" \" + dossier.getDoctorCreate().getUtilisateur().getPrenom() : \"\")")
     DossierViewDto toDossierViewDto(Dossier dossier);
 
 
