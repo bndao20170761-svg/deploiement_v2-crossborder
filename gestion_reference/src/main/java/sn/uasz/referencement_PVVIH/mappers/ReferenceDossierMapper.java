a5b146cdcdb5868c5990fa0f2ad26407f0ceb266
Babacar Ndao <babacarndao1011@gmail.com>
2026-04-21 04:35:01 +0000

    mise à jour

--- gestion_reference/src/main/java/sn/uasz/referencement_PVVIH/mappers/ReferenceDossierMapper.java
+++ gestion_reference/src/main/java/sn/uasz/referencement_PVVIH/mappers/ReferenceDossierMapper.java
@@ -2,14 +2,11 @@ package sn.uasz.referencement_PVVIH.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 import sn.uasz.referencement_PVVIH.dtos.ReferenceDossierDto;
 import sn.uasz.referencement_PVVIH.entities.ReferenceDossier;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface ReferenceDossierMapper {
-    
-    ReferenceDossierMapper INSTANCE = Mappers.getMapper(ReferenceDossierMapper.class);
     
     @Mapping(target = "dateCreation", expression = "java(java.time.LocalDateTime.now())")
     ReferenceDossier dtoToEntity(ReferenceDossierDto dto);
