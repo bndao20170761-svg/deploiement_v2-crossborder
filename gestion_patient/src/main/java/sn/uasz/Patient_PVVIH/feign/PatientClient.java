fcf500472a3b332142d50d64bd89f527fbd6281b
BabacarNdaoKgl <ndaocounda1@icloud.com>
2026-04-23 02:14:23 +0000

    mise à jour endpoints

--- gestion_patient/src/main/java/sn/uasz/Patient_PVVIH/feign/PatientClient.java
+++ gestion_patient/src/main/java/sn/uasz/Patient_PVVIH/feign/PatientClient.java
@@ -8,9 +8,9 @@ import sn.uasz.Patient_PVVIH.dtos.PatientDto;
 import java.util.List;
 
 @FeignClient(
-        name = "gestion-user",
-    url = "${feign.user-service.url:http://gestion-user:8080}",
-    configuration = FeignClientConfig.class
+        name = "gestion-user-patient",
+        url = "${feign.user-service.url:http://gestion-user:8080}",
+        configuration = FeignClientConfig.class
 )
 public interface PatientClient {
 
