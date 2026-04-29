76778bc18d996c67d08dcad84a0df6f141b5e380
Babacar Ndao <babacarndao1011@gmail.com>
2026-04-29 01:50:33 +0000

    mise à jour dossier reference front

--- a_reference_front/src/services/referenceService.js
+++ a_reference_front/src/services/referenceService.js
@@ -4,66 +4,66 @@ import api from './api';
 
 // Obtenir les références envoyées
 export const getSentReferences = async () => {
-  const response = await api.get('/references/sent');
+  const response = await api.get('/references-dossiers/envoyees');
   return response.data;
 };
 
 // Obtenir les références reçues
 export const getReceivedReferences = async () => {
-  const response = await api.get('/references/received');
+  const response = await api.get('/references-dossiers/recues');
   return response.data;
 };
 
 // Créer une nouvelle référence
 export const createReference = async (referenceData) => {
-  return await api.post('/references/New_ref', referenceData);
+  return await api.post('/references-dossiers', referenceData);
 };
 
 // Récupérer toutes les références
 export const getAllReferences = async () => {
-  return await api.get('/references');
+  return await api.get('/references-dossiers');
 };
 
 // Récupérer une référence par ID
 export const getReferenceById = async (id) => {
-  return await api.get(`/references/${id}`);
+  return await api.get(`/references-dossiers/${id}`);
 };
 
 // Validation d'une référence
 export const validateReference = async (id) => {
-  const response = await api.put(`/references/${id}/validation`, {});
+  const response = await api.put(`/references-dossiers/${id}/validation`, {});
   return response.data;
 };
 
 // Contre-référence (feedback)
 export const feedbackReference = async (id) => {
-  const response = await api.put(`/references/contreReference/${id}`, {});
+  const response = await api.put(`/references-dossiers/contreReference/${id}`, {});
   return response.data;
 };
 
 export const countRecuesNonLues = async () => {
-  const response = await api.get('/references/count/recues-non-lues');
+  const response = await api.get('/references-dossiers/count/recues-non-lues');
   return response.data; // renvoie un nombre
 };
 
 //assistant send
 export const getCountReferencesEnvoyeesParAssistant = async () => {
-  const response = await api.get('/references/count-envoyees-assistant');
+  const response = await api.get('/references-dossiers/count-envoyees-assistant');
   return response.data; // renvoie un nombre
 };
 
 export const countEnvoyees = async () => {
-  const response = await api.get('/references/count/envoyees');
+  const response = await api.get('/references-dossiers/count/envoyees');
   return response.data; // renvoie un nombre
 };
 
 // Mettre à jour une référence
 export const updateReference = async (id, referenceData) => {
-  const response = await api.put(`/references/${id}`, referenceData);
+  const response = await api.put(`/references-dossiers/${id}`, referenceData);
   return response.data;
 };
 
 // Supprimer une référence
 export const deleteReference = async (id) => {
-  return await api.delete(`/references/${id}`);
+  return await api.delete(`/references-dossiers/${id}`);
 };
