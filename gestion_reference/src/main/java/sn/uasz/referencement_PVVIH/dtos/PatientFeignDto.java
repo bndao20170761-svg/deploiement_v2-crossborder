941897d60df2405a096485eb4f5f3fbd1272beab
BabacarNdaoKgl <ndaocounda1@icloud.com>
2026-05-02 01:16:43 +0000

    mise à jour

--- gestion_reference/src/main/java/sn/uasz/referencement_PVVIH/dtos/PatientFeignDto.java
+++ gestion_reference/src/main/java/sn/uasz/referencement_PVVIH/dtos/PatientFeignDto.java
@@ -1,6 +1,6 @@
 package sn.uasz.referencement_PVVIH.dtos;
 
-import com.fasterxml.jackson.annotationxv.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
